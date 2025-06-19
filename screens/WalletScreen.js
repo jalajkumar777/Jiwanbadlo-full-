e1f63f3a09c32bf4e0fd27cddf1d5fcc9773dc40
jalajkumar777 <reelscractor@gmail.com>
2025-06-19 08:09:26 +0530

    Create WalletScreen.js

---
+++ screens/WalletScreen.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const WalletScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>💰 Million Wallet</Text>
+      <Text style={styles.subtitle}>Followers, Views, Rewards Info</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+  },
+});
+
+export default WalletScreen;
